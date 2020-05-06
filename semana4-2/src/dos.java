3d88aa0bb90a13cb0a9e4dca5158376ec9abad97
zafiro <you@example.com>
2020-05-06 02:52:45 +0200

    primer

---
+++ semana4-2/src/dos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class dos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner (System.in); 
+		System.out.println("ingrese el primer numero");
+		double a=scan.nextDouble();
+	if (a>0){
+		System.out.println("es positivo");
+	}else {
+		System.out.println("es negativo");
+	}
+		
+	}
+
+}
